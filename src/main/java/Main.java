48b8d1767f47271808ec1f2c8a9d1df83686b39e
Volodymyr Holichenko <vladimirgolicenko154@gmail.com>
2024-01-17 09:04:22 +0200

    driver agnostic pooled data source PoC

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import javax.sql.DataSource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataSource dataSource = initPostgresPooledDatasource();
+        var start = System.nanoTime();
+        for (int i = 0; i < 10_000; i++) {
+            try (var connection = dataSource.getConnection();
+                 var statement = connection.prepareStatement("select now() as time")) {
+                statement.executeQuery();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        var end = System.nanoTime();
+        System.out.println((end - start) / 1_000_000 + "ms");
+
+    }
+
+    private static DataSource initPostgresPooledDatasource() {
+        return new PooledDataSource("jdbc:postgresql://localhost:5432/postgres", "postgres", "admin", 10);
+    }
+
+    private static DataSource initMysqlPooledDataSource() {
+        return new PooledDataSource("jdbc:mysql://localhost:3306", "root", "admin", 10);
+    }
+}
---
+++ src/main/java/PooledDataSourceCreationException.java
@@ -0,0 +1,9 @@
+public class PooledDataSourceCreationException extends RuntimeException {
+    public PooledDataSourceCreationException(String message) {
+        super(message);
+    }
+
+    public PooledDataSourceCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
